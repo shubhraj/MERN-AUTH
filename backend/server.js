03a0149d6d238feaa37398e6a0d98cf6c09c7017
patilsh <shubham.patil@sincrodigital.com>
2023-05-31 08:49:53 +0530

    added a error handler

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotevn from "dotenv";
 dotevn.config();
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
+
 const PORT = process.env.PORT || 5000;
 
 import userRoutes from "./routes/userRoutes.js";
@@ -12,6 +15,9 @@ app.use('/api/users', userRoutes);
 app.get('/', (req, res) => {
     res.send('server is ready');    
 })
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`server started on ${PORT}`);
